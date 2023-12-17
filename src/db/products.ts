4af75464294e9da480d3b8af7bab57efaa49e6d0
Alex Nacimento <alexdavi150@gmail.com>
2023-12-17 18:57:41 -0300

    style: delete unecessary comments

--- src/db/products.ts
+++ src/db/products.ts
@@ -7,16 +7,3 @@ const ProductSchema = new mongoose.Schema({
 })
 
 export const ProductModel = mongoose.model('Product', ProductSchema)
-
-// export const getProducts = () => ProductModel.find()
-
-// export const createProduct = (data: Record<string, any>) => {
-//   new ProductModel(data).save()
-//     .then(product => product.toObject())
-// }
-
-// export const deleteProduct = (id: string) => ProductModel.findOneAndDelete({ _id: id })
-
-// export const updateProduct = (id: string, newData: Record<string, any>) => {
-//   ProductModel.findByIdAndUpdate(id, newData)
-// }
